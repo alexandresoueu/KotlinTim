5df914de6530c594e0978cb73a694d6bca0863f2
AlexandreSouEu <calexandredev@gmail.com>
2019-05-13 11:46:27 -0300

    Kotlin Challenges

---
+++ src/academy/learnprogramming/textadventure/challenge.kt
@@ -0,0 +1,25 @@
+package academy.learnprogramming.textadventure
+
+fun main() {
+    val helloOne = "Hello"
+    val helloTwo = "Hello"
+
+    println("HelloOne os referentially equal helloTwo: ${helloOne === helloTwo}")
+
+    println("HelloOne os structurally equal helloTwo: ${helloOne == helloTwo}")
+
+    var num = 2988
+
+    val text: Any = "the Any type ****"
+    if(text is String) {
+        println(text.toUpperCase())
+    }
+
+    println("""   1
+        |  11
+        | 111
+        |1111
+    """.trimMargin("1"))
+
+
+}
