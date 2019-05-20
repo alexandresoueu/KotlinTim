da789d0630366a7a3a429e05ad8d5bf3eaa2eacf
AlexandreSouEu <calexandredev@gmail.com>
2019-05-20 15:18:58 -0300

    Example Access Modifier

--- src/AccessModifiers/AccessModifiers.kt
+++ src/AccessModifiers/AccessModifiers.kt
@@ -1,10 +1,36 @@
 package AccessModifiers
 
 fun main() {
-    val dori = Nemo()
-    println(dori)
+    val dori = Nemo("Zaira")
+    println(dori.firstName)
+
+    val dolly = Nemo("Bob")
+    println(dolly.firstName)
+    println(dolly.fullTime)
+
+    val MichaelDouglas = Nemo("Ghost", false)
+    println(MichaelDouglas.firstName)
+    println(MichaelDouglas.fullTime)
+
+    println(Demo().dummy)
+
 }
 
-private class Nemo {
+class Nemo (val firstName: String, var fullTime: Boolean = true) {
 
+    //val firstName: String = firstName
+
+    /*var fullTime: Boolean = true
+
+    constructor(firstName: String, fullTime: Boolean): this(firstName) {
+        this.fullTime = fullTime
+    }*/
+}
+
+class Demo {
+    val dummy: String
+
+    constructor(){
+        dummy = "HEY JUDE"
+    }
 }
