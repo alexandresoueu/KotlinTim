6d89f6aed003f4b5dabd29107e7925d7d5a53127
Alexandre DEV <calexandredev@gmail.com>
2019-05-25 20:23:29 -0300

    Example INHERITANCE

---
+++ src/Inheritance/Printer.kt
@@ -0,0 +1,42 @@
+package Inheritance
+
+fun main() {
+    val laserPrinter = LaserPrinter("Dori is blue fish! #3F46BF", 178)
+    laserPrinter.printModel()
+
+    SomethingElse("Eai Rapaziada!!!!")
+}
+
+abstract  class Printer(val modelName: String) {
+    open fun printModel() = println("The model name of this printer is $modelName") //allows subclassing a class or overriding a member
+    abstract fun bestSellingPrice(): Double //marks a class or member as abstract
+}
+
+open class LaserPrinter(modelName: String, ppm: Int): Printer(modelName) {
+    final override fun printModel() = println("The model name of this laser printer is $modelName")
+    override fun bestSellingPrice(): Double = 9.99 //marks a member as an override of a superclass member
+}
+
+class SpecialLaserPrinter(modelName: String, ppm: Int): LaserPrinter(modelName, ppm) {
+
+}
+
+open class Something {
+    val someProperty: String
+
+    constructor(someParameter: String) {
+        someProperty = someParameter
+        println("I'm in the parent's constructor........")
+    }
+}
+
+class SomethingElse: Something {
+    constructor(someOtherParameter: String): super(someOtherParameter) {
+        println("I'm in the child's constructor***********")
+    }
+}
+
+/*
+open data class DataClass(val number: Int) {
+
+}*/
