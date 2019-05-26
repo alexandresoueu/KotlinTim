bed53b3fb53b8686b29bd2d29ee068da622a5abf
Alexandre DEV <calexandredev@gmail.com>
2019-05-26 19:34:05 -0300

    Example of companion

--- src/Inheritance/CompanyCommunications.kt
+++ src/Inheritance/CompanyCommunications.kt
@@ -5,6 +5,15 @@ import java.time.Year
 fun main() {
     println(CompanyCommunications.getTagLine())
     println(CompanyCommunications.getCopurightLine())
+    println("========================================")
+    println(SomeClass.accessPrivateVar())
+    println("========================================")
+
+    val someClassOne = SomeClass.justAssign("nemo living in ocean")
+    val someClassTwo = SomeClass.upperOrLowerCase("Dori swimming in oceand!", false)
+
+    println(someClassOne.someString)
+    println(someClassTwo.someString)
 }
 
 object CompanyCommunications {
@@ -12,4 +21,22 @@ object CompanyCommunications {
 
     fun getTagLine() = "Our Company"
     fun getCopurightLine() = "Company \u00A9 $currentYear Our Company."
+}
+
+class SomeClass private constructor(val someString: String) {
+
+    companion object {
+        private val privateVar = 6
+
+        fun accessPrivateVar() = "I'm accessing privateVar $privateVar"
+
+        fun justAssign(str: String) = SomeClass(str)
+        fun upperOrLowerCase(str: String, lowerCase: Boolean): SomeClass {
+            if (lowerCase) {
+                return SomeClass(str.toLowerCase())
+            } else {
+                return SomeClass(str.toUpperCase())
+            }
+        }
+    }
 }
---
+++ src/Inheritance/javacode/SomeOtherClass.java
@@ -0,0 +1,7 @@
+package Inheritance.javacode;
+
+public class SomeOtherClass {
+    public void someOtherMethod() {
+        SomeClass.accessPrivateVar();
+    }
+}
