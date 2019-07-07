1b23bbc261e78abed3defad45b6757d093fc74e4
Alexandre DEV <calexandredev@gmail.com>
2019-07-06 23:06:22 -0300

    Create examples of collection functions and merged/mutable

---
+++ src/Lambdas/Lists.kt
@@ -0,0 +1,42 @@
+package Lambdas
+
+fun main() {
+    val strings = listOf("spring", "fall", "summer", "winter", "spring")
+    val colorList = listOf("blue", "white", "orange", "gray", "white", "orange")
+
+    println("=============STRINGS===============")
+    println(strings.last())
+    println(strings.asReversed())
+
+    println(strings.getOrNull(5))
+
+    /*if (strings.size > 5) {
+        println(strings[5])
+    }*/
+
+    println("=============INTS===============")
+
+    val ints = listOf(1, 2, 3, 4, 5)
+    println(ints.max())
+    println(colorList.zip(strings))
+
+    println("=============MERGED AND NO DUPLICATE===============")
+
+    val mergedLists = listOf(strings, colorList)
+    println(mergedLists)
+
+    val combinerList = strings + colorList
+    println(combinerList)
+
+    val noDupsList = colorList.union(strings)
+    println(noDupsList)
+
+    val noDupColors = colorList.distinct()
+    println(noDupColors)
+
+    println("=============MUTABLE===============")
+
+    val mutableSeasons = strings.toMutableList()
+    mutableSeasons.add("some other season")
+    println(mutableSeasons)
+}
